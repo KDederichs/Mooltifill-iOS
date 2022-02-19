479a359bc4f1ad7803368f8781000718527d47fa
Kai Dederichs <kai.dederichs@protonmail.com>
2022-02-19 11:51:32 +0100

    Initial Commit

---
+++ Mooltifill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mooltifill
+//
+//  Created by Kai Dederichs on 19.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
